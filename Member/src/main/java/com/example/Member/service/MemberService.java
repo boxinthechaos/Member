b129a05394cc4aa9344278070add08c891dfc51e
boxinthechaos <kdmwp310@gmail.com>
2024-11-27 19:48:17 +0900

    update Login function

---
+++ Member/src/main/java/com/example/Member/service/MemberService.java
@@ -0,0 +1,53 @@
+package com.example.Member.service;
+
+import com.example.Member.dto.MemberDto;
+import com.example.Member.entity.MemberEntity;
+import com.example.Member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.ArrayList;
+
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public void save(MemberDto memberDto){
+        MemberEntity memberEntity = MemberEntity.toMemberEntity(memberDto);
+        memberRepository.save(memberEntity);
+    }
+    public MemberDto login(MemberDto memberDTO){ //entity객체는 service에서만
+        Optional<MemberEntity> byMemberEmail = memberRepository.findByMemberEmail(memberDTO.getMemberEmail());
+        if(byMemberEmail.isPresent()){
+            // 조회 결과가 있다
+            MemberEntity memberEntity = byMemberEmail.get(); // Optional에서 꺼냄
+            if(memberEntity.getMemberPassword().equals(memberDTO.getMemberPassword())) {
+                //비밀번호 일치
+                //entity -> dto 변환 후 리턴
+                MemberDto dto = MemberDto.toMemberDto(memberEntity);
+                return dto;
+            } else {
+                //비밀번호 불일치
+                return null;
+            }
+        } else {
+            // 조회 결과가 없다
+            return null;
+        }
+    }
+    public List<MemberDto> findAll() {
+        List<MemberEntity> memberEntityList = memberRepository.findAll();
+        //Controller로 dto로 변환해서 줘야 함
+        List<MemberDto> memberDTOList = new ArrayList<>();
+        for (MemberEntity memberEntity : memberEntityList){
+            memberDTOList.add(MemberDto.toMemberDto(memberEntity));
+
+        }
+        return memberDTOList;
+
+    }
+}
